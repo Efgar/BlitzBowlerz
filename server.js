13f546cf4625473dc01a209da3dde3f64923d86c
Efgar <edguitar000@gmail.com>
2020-11-25 17:03:56 +0100

    pckg

--- server.js
+++ server.js
@@ -4,20 +4,20 @@ const path = require('path');
 
 const app = express();
 
-/*
+const path = require('path');
 app.get('/*', function(req,res) {
-        res.sendFile(path.join(__dirname+'/dist/index.html'));
+        res.sendFile(path.join(__dirname+'/dist/BlitzBowlers/index.html'));
     }
 );
-*/
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/'));
+app.use(express.static(__dirname + '/dist/BlitzBowlers'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/index.html'));
+res.sendFile(path.join(__dirname+'/dist/BlitzBowlers/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
